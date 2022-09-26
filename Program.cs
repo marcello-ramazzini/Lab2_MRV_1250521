711db16e411c6848b951763cadb3d49114f80d67
Estudiante <Estudiante@T203-01-2022>
2022-09-26 21:58:47 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroEntero;
+
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Por favor, ingrese un cualquier nùmero entero");
+
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+if (numeroEntero > 0)
+{
+    Console.WriteLine("Numero positivo");
+}
+else if (numeroEntero < 0)
+{
+    Console.WriteLine("Nùmero negativo");
+}
+else
+{
+    Console.WriteLine("Cero");
+}
+
+Console.WriteLine(numeroEntero);
